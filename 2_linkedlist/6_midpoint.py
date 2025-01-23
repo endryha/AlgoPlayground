ab7c2a6ccb4e84cd4d4fd5c0fd518411a64409c9
endryha <endryha@gmail.com>
2025-01-22 19:19:22 -0600

    find mid point solution

---
+++ 2_linkedlist/6_midpoint.py
@@ -0,0 +1,52 @@
+from linkedlist import create_linkedlist, print_linkedlist, ListNode
+
+
+def find_midpoint_naive(head: ListNode) -> ListNode:
+    current = head
+    length = 0
+    while current:
+        length += 1
+        current = current.next
+
+    midpoint = int(length / 2)
+    counter = 0
+    current = head
+    while current:
+        if counter == midpoint:
+            return current
+        else:
+            current = current.next
+        counter += 1
+
+
+def find_midpoint_2_pointers(head: ListNode) -> ListNode:
+    fast = head
+    slow = head
+
+    while True:
+        if fast is None or fast.next is None:
+            return slow
+
+        slow = slow.next
+        fast = fast.next.next
+
+
+def test_find_midpoint(head: ListNode):
+    print_linkedlist(head)
+    print()
+    print("Solution naive")
+    print("Midpoint:", find_midpoint_naive(head))
+    print()
+    print("Solution 2 pointers")
+    print("Midpoint:", find_midpoint_2_pointers(head))
+
+
+head = create_linkedlist(9)
+print("Test odd list:")
+test_find_midpoint(head)
+
+print()
+
+print("Test even list:")
+head = create_linkedlist(8)
+test_find_midpoint(head)
